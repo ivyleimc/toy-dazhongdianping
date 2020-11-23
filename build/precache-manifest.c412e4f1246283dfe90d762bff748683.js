28fc8a687239ea5d9bee73bdd28f0fa402407f45
ivyleimc <ivy_xiaocheng@163.com>
2020-11-23 21:31:50 +0800

    add build folder

---
+++ build/precache-manifest.c412e4f1246283dfe90d762bff748683.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9afd327a160f7c8517fea02821369da6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba46c5e192b9df7da086",
+    "url": "/static/js/2.9d2ba051.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9d2ba051.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61f5c690e94b329825ac",
+    "url": "/static/js/main.9fb9a800.chunk.js"
+  },
+  {
+    "revision": "f5860f684da3b84c6488",
+    "url": "/static/js/runtime-main.5477c232.js"
+  }
+]);
